4b84d8a4ddae349157cd76377dc38074d68eb73b
s-kngstn <s.kngstn@protonmail.com>
2022-01-12 16:10:51 +0000

    no longer importing and mutating excel file - deploment testing

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,8 +1,14 @@
 import styles from "../../styles/Home.module.css";
 
-const Checkbox = ({ type = 'checkbox', name, checked = false, onChange }) => {
-  console.log("Checkbox: ", name, checked);
-
-return (<input  className={styles.checkBox} type={type} name={name} checked={checked} onChange={onChange} /> )
-}
+const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => {
+  return (
+    <input
+      className={styles.checkBox}
+      type={type}
+      name={name}
+      checked={checked}
+      onChange={onChange}
+    />
+  );
+};
 export default Checkbox;
